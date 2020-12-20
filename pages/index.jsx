d3c565624b4cd43a889742c0c8365e7fa88eec6a
John Mejia <jhondario.09@gmail.com>
2020-12-20 17:02:43 -0500

    Added search logic and global styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,110 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-const HomePage = () => {
+// Components
+import Search from '../components/Search';
+
+// CSS
+import styles from './index.module.scss';
+
+const initialData = {
+    usuariosResult: [],
+    tablerosResult: [],
+    fuentesResult: [],
+    conciliacionesResult: [],
+    result: []
+};
+
+const getPromise = (url, type) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                resolve({type, ...data});
+            });
+    });
+};
+
+const App = () => {
+    const [allData, setAllData] = useState([]);
+    const [search, setSearch] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+
+    useEffect(() => {
+        const URL = 'http://localhost:3000/api';
+
+        const p1 = getPromise(URL + '/conciliaciones/', 'conciliaciones');
+        const p2 = getPromise(URL + '/fuentes/', 'fuentes');
+        const p3 = getPromise(URL + '/tableros/', 'tableros');
+        const p4 = getPromise(URL + '/usuarios/', 'usuarios');
+
+        const promises = Promise.all([p1, p2, p3, p4]);
+
+        promises.then(result => {
+            setAllData(result);
+            console.log('result', result)
+        })
+        .catch(err => console.log(err));
+    }, []);
+
+    const searchByType = (data, searchInput, type) => {
+        data.forEach((infoItem, i) => {
+            if (type !== 'usuariosResult' && infoItem.description.includes(searchInput.toLowerCase())) {
+                initialData[type].push(infoItem);
+            }
+        });
+    }
+
+    const filterData = (searchInput) => {
+        initialData.conciliacionesResult = [];
+        initialData.fuentesResult = [];
+        initialData.tablerosResult = [];
+        initialData.usuariosResult = [];
+        allData.forEach(item => {
+            switch(item.type) {
+                case 'conciliaciones':
+                    searchByType(item.data, searchInput, 'conciliacionesResult');
+                    break;
+                case 'fuentes':
+                    searchByType(item.data, searchInput, 'fuentesResult');
+                    break;
+                case 'tableros':
+                    searchByType(item.data, searchInput, 'tablerosResult');
+                    break;
+                case 'usuarios':
+                    searchByType(item.data, searchInput, 'usuariosResult');
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        initialData.result = [
+            {type: 'conciliaciones', data: [...initialData.conciliacionesResult]},
+            {type: 'fuentes', data: [...initialData.fuentesResult]},
+            {type: 'tableros', data: [...initialData.tablerosResult]},
+            {type: 'usuarios', data: [...initialData.usuariosResult]}
+        ];
+        setSearchResult(initialData.result);
+        console.log('filtered', initialData.result)
+    };
+
+    const handlerSearch = (e) => {
+        setSearch(e.target.value);
+        filterData(search);
+    }
+
     return (
-        <div>Hello!</div>
+        <div className={styles.container}>
+            <section className={styles.content}>
+                <Search
+                    search={search}
+                    searchResult={searchResult}
+                    handlerSearch={handlerSearch} />
+            </section>
+        </div>
     );
 };
 
-export default HomePage;
+export default App;
