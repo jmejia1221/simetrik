e8cc5b86f7aac84def6c8324c784d8a6d4231ecb
John Mejia <jhondario.09@gmail.com>
2020-12-21 01:08:18 -0500

    Ignoring .next folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1dd843d6b5a78cd7d947";
+/******/ 	var hotCurrentHash = "de4d55e2c57f49b1dcbe";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
